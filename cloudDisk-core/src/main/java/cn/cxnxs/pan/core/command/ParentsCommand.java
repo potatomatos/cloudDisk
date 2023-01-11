e9fc3fae4475e05dcb0272f3cd55710ac03c3809
mengjinyuan <782399171@qq.com>
2023-01-12 00:51:50 +0800

    获取文件层级路径

--- cloudDisk-core/src/main/java/cn/cxnxs/pan/core/command/ParentsCommand.java
+++ cloudDisk-core/src/main/java/cn/cxnxs/pan/core/command/ParentsCommand.java
@@ -6,8 +6,8 @@ import cn.cxnxs.pan.core.service.VolumeHandler;
 import com.alibaba.fastjson.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * 获得所有上级
@@ -16,14 +16,27 @@ public class ParentsCommand extends AbstractJsonCommand implements ElfinderComma
     @Override
     protected void execute(ElfinderStorage elfinderStorage, HttpServletRequest request, JSONObject json) throws Exception {
         final String target = request.getParameter(ElFinderConstants.ELFINDER_PARAMETER_TARGET);
-
-        Map<String, VolumeHandler> files = new HashMap<>();
+        Object[] objects = new Object[]{};
+        List<VolumeHandler> files = new ArrayList<>();
         VolumeHandler volumeHandler = findTarget(elfinderStorage, target);
-        do {
+        if (volumeHandler.isRoot()) {
+            json.put(ElFinderConstants.ELFINDER_PARAMETER_TREE, objects);
+            return;
+        }
+        while (!volumeHandler.isRoot()) {
+            files.add(volumeHandler);
             volumeHandler = volumeHandler.getParent();
-            addSubFolders(files, volumeHandler);
-        }while (!volumeHandler.isRoot());
+        }
+        Collections.reverse(files);
+        objects = files.stream().map(handler -> {
+            try {
+                return getTargetInfo(request, handler);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).toArray();
 
-        json.put(ElFinderConstants.ELFINDER_PARAMETER_TREE, buildJsonFilesArray(request, files.values()));
+        json.put(ElFinderConstants.ELFINDER_PARAMETER_TREE, objects);
     }
 }
