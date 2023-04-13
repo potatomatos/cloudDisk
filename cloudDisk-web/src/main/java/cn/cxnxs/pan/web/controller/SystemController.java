82abf65cb0031408c5f2d521b8b9f212532ae3db
mengjinyuan <782399171@qq.com>
2023-04-14 01:06:13 +0800

    优化配置

--- cloudDisk-web/src/main/java/cn/cxnxs/pan/web/controller/SystemController.java
+++ cloudDisk-web/src/main/java/cn/cxnxs/pan/web/controller/SystemController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -34,9 +35,11 @@ public class SystemController {
      * 获取token
      */
     @GetMapping("/getAccessToken")
-    public Result<Map<String, String>> getAccessToken(String code,String clientId,String redirectUri) {
-        log.info("code:{}", code);
-        if (StringUtil.isEmpty(code)){
+    public Result<Map<String, String>> getAccessToken(@RequestParam("code") String code,
+                                                      @RequestParam("clientId")String clientId,
+                                                      @RequestParam("redirectUri")String redirectUri) {
+        log.info("code:{},clientId:{},redirectUri:{}", code, clientId, redirectUri);
+        if (StringUtil.isEmpty(code)) {
             return Result.failure("code不能为空！");
         }
         log.info("------开始获取token------");
