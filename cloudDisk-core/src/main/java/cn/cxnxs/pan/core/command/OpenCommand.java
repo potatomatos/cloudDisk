dd39a454aabdb4ba6ce0a70f101aa410c4de720a
mengjinyuan <782399171@qq.com>
2023-04-22 23:32:47 +0800

    添加返回顶级目录

--- cloudDisk-core/src/main/java/cn/cxnxs/pan/core/command/OpenCommand.java
+++ cloudDisk-core/src/main/java/cn/cxnxs/pan/core/command/OpenCommand.java
@@ -24,6 +24,12 @@ public class OpenCommand extends AbstractJsonCommand implements ElfinderCommand 
         if (init) {
             json.put(ElFinderConstants.ELFINDER_PARAMETER_API, ElFinderConstants.ELFINDER_VERSION_API);
             json.put(ElFinderConstants.ELFINDER_PARAMETER_NETDRIVERS, new Object[0]);
+            for (Volume volume : elfinderStorage.getVolumes()) {
+                VolumeHandler root = new VolumeHandler(volume.getRoot(), elfinderStorage);
+                files.put(root.getHash(), root);
+            }
+            this.buildResult(null,files,json,request);
+            return;
         }
 
         if (tree) {
@@ -37,20 +43,26 @@ public class OpenCommand extends AbstractJsonCommand implements ElfinderCommand 
         VolumeHandler cwd = findCwd(elfinderStorage, target);
         files.put(cwd.getHash(), cwd);
         addChildren(files, cwd);
+        this.buildResult(cwd,files,json,request);
+    }
 
+
+    private void buildResult(VolumeHandler cwd,Map<String, VolumeHandler> files,JSONObject json,HttpServletRequest request) throws Exception{
         Object[] objects = buildJsonFilesArray(request, files.values());
         List<Object> filesList = new ArrayList<>(Arrays.asList(objects));
-        String hash = cwd.getHash();
-        for(Object obj : filesList){
-            HashMap<String,Object> map = (HashMap<String, Object>) obj;
-            String strHash = map.get("hash").toString();
-            if(Objects.equals(hash, strHash)){
-                json.put(ElFinderConstants.ELFINDER_PARAMETER_CWD,map);
-                filesList.remove(map);
-                break;
-            }
-        }
         json.put(ElFinderConstants.ELFINDER_PARAMETER_FILES, filesList);
-        json.put(ElFinderConstants.ELFINDER_PARAMETER_OPTIONS, getOptions(cwd));
+        if (cwd!=null) {
+            String hash = cwd.getHash();
+            for(Object obj : filesList){
+                HashMap<String,Object> map = (HashMap<String, Object>) obj;
+                String strHash = map.get("hash").toString();
+                if(Objects.equals(hash, strHash)){
+                    json.put(ElFinderConstants.ELFINDER_PARAMETER_CWD,map);
+                    filesList.remove(map);
+                    break;
+                }
+            }
+            json.put(ElFinderConstants.ELFINDER_PARAMETER_OPTIONS, getOptions(cwd));
+        }
     }
 }
