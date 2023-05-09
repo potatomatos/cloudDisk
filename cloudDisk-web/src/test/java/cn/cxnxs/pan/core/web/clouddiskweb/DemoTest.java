7c3a5cc16e1ab47524695e42b2866c8219e39b9c
potatomatos <782399171@qq.com>
2023-05-09 18:19:30 +0800

    测试

--- cloudDisk-web/src/test/java/cn/cxnxs/pan/core/web/clouddiskweb/DemoTest.java
+++ cloudDisk-web/src/test/java/cn/cxnxs/pan/core/web/clouddiskweb/DemoTest.java
@@ -1,12 +1,15 @@
 package cn.cxnxs.pan.core.web.clouddiskweb;
 
-import org.apache.commons.fileupload.FileItemStream;
 import org.junit.Test;
 
 public class DemoTest {
 
     @Test
     public void test() {
-        System.out.println(FileItemStream.class.getName());
+        String path = "279:\\/PC/新建文件夹";
+        int index = path.indexOf(":\\");
+        System.out.println(index);
+        System.out.println(path.substring(0,index));
+        System.out.println(path.substring(index+2));
     }
 }
